0d025c8d1e099c962d0966f6e9274d714f6f99ea
iam-tsr <tusharsoni.info@gmail.com>
2025-09-28 14:52:18 +0530

    Prompt Improve

--- app/model/prompt.py
+++ app/model/prompt.py
@@ -10,7 +10,7 @@ def gen_prompt():
 
     prompt = f"""
 
-    Generate an image of a person doing "{event}" at {place}.
+    Generate a hyper-realistic image of a person doing "{event}" at {place}, there should be some other peoples too with the person engaging.
     The image should be in a realistic style with high detail and vibrant colors.
     Try to match the person's appearance as possible to the provided image but clothing can be varied, depends on the {place}.
 
